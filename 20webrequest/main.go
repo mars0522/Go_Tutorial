b70bd7050ed3db26b5ff4b5090ee0f516d9f2293
varun pratap <varun.singh01@indiamart.com>
2023-05-28 20:42:03 +0530

    http request using form data

--- 20webrequest/main.go
+++ 20webrequest/main.go
@@ -15,10 +15,12 @@ func main() {
 
 	defer res.Body.Close()
 
+	// Reading Body from response which contains string  content in Bytes format
 	bodyinbytes, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		panic(err)
 	}
+	// Converting the bytes into a string
 	content := string(bodyinbytes)
 	fmt.Println(content)
 }
